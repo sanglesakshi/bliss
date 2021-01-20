0ba08307d7bbb060c63bb20e2a5a825c8a3f866f
Sakshi Sangale <67558676+sanglesakshi@users.noreply.github.com>
2021-01-20 18:40:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$("img").click(function(event){
+    $(this).fadeToggle();
+    $(this).fadeToggle("slow");
+});
+
+/*$(".social-icon").hover(function(event) {
+  $(this).css("color", "#1f6f8b");
+});*/
+
+/*$("a").click(function pop_up(){
+  var begin_size = 10;
+  var end_size = 30;
+  var speed = 3000;
+
+  $("a").css("font-size", begin_size).animate({"font-size" : end_size}, speed);
+pop_up();
+});*/
